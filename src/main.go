125647196b8b3abfedd9f08bb2225d084235d363
Edouard127 <46357922+Edouard127@users.noreply.github.com>
2022-12-22 18:56:29 -0500

    Fix: Deployment

--- src/main.go
+++ src/main.go
@@ -1,10 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"edouard127/copingheimer/src/server"
+	"edouard127/copingheimer/src/worker"
+	"flag"
+)
 
 func main() {
-	fmt.Println(`
-		This file is not for running the server or the worker.
-		If you would like some examples, please check the /src/example folder.
-    `)
+	// Parse flags
+	types := flag.String("type", "server", "Type of the program to run (server or worker)")
+	flag.Parse()
+	if *types == "server" {
+		server.StartServer()
+	} else if *types == "worker" {
+		worker.StartWorker()
+	}
 }
